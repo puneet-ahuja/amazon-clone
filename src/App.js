7830b3cdb2206c14f615ed88a97859034145cbed
Puneet Ahuja <puneet.ahuja@olx.com>
2020-11-25 19:59:47 +0530

    Added Basic Routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="app">
-    </div>
+    <Router>
+      <div className="app">
+
+        <Switch>
+          <Route path='/checkout'>
+            <h1>Checkout Component</h1>
+          </Route>
+          <Route path='/login'>
+            <h1>Login Component</h1>
+          </Route>
+          <Route path='/'>
+            <h1>Homepage</h1>
+          </Route>
+
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
